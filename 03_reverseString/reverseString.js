81192644fe49d38cd72aa8978923ef9eba925a46
Rohan Dalal <rohan.d.dalal@gmail.com>
2024-06-23 15:29:00 -0400

    03 reverseString complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(reverse) {
+    let returnString = "";
 
+    for(let i = reverse.length - 1; i >= 0 ; i--) {
+        returnString += reverse[i]
+    }
+
+    return returnString;
 };
 
 // Do not edit below this line
