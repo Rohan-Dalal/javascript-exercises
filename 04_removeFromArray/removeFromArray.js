c1565e7924381b8fee3a01973ab77a7a620ad8bc
Rohan Dalal <rohan.d.dalal@gmail.com>
2024-06-24 15:33:54 -0400

    04_removeFromArray complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    let newArray = array.filter((x) => !(toRemove.includes(x)));
+    return newArray;
 };
 
 // Do not edit below this line
