4c5e69697f94e1a9b1db9673bb34333619b47b29
Rohan Dalal <rohan.d.dalal@gmail.com>
2024-06-30 12:24:28 -0400

    12_findTheOldest complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((greatest, current) => {
+        let yearOfDeath;
 
+        if(current.yearOfDeath === undefined) {
+            yearOfDeath = new Date().getFullYear();
+        } else {
+            yearOfDeath = current.yearOfDeath;
+        }
+
+        let greatestYearOfDeath;
+        if(greatest.yearOfDeath === undefined) {
+            greatestYearOfDeath = new Date().getFullYear();
+        } else {
+            greatestYearOfDeath = greatest.yearOfDeath;
+        }
+
+        if(yearOfDeath - current.yearOfBirth > greatestYearOfDeath - greatest.yearOfBirth) {
+            return current;
+        }
+        return greatest;
+    })
 };
 
 // Do not edit below this line
